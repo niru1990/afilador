b3b9e929d4cd99363da6a8538d7125a8dcc00d15
Nico <nicolas.reyes90@gmail.com>
2022-01-29 14:07:19 -0300

    Commit inicial

---
+++ src/main/java/afilando/programacion/Afilador/AfilandoProgramacionApplication.java
@@ -0,0 +1,13 @@
+package afilando.programacion.Afilador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfilandoProgramacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfilandoProgramacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/afilando/programacion/Afilador/AfilandoProgramacionApplicationTests.java
@@ -0,0 +1,13 @@
+package afilando.programacion.Afilador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfilandoProgramacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
